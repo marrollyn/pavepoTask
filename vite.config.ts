705e212aeeac740970f9ac49c7f037de7db9cfd8
Marina Moiseenko <marina.moiseenko@list.ru>
2025-04-10 19:35:50 +0300

    feat: add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,17 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
+export default defineConfig(({ }) => {
+	const deployTarget = process.env.VITE_DEPLOY_TARGET;
+	const base = deployTarget === 'github' ? '/pavepoTask/' : '/';
+
+	return {
+		base,
+		plugins: [react()],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+			},
 		},
-	},
+	};
 });
